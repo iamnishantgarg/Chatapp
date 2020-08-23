8743cf9989c75d3acbb2df3b36fe306cb8009cb9
Nishant garg <iamnishantgarg@gmail.com>
2020-08-23 13:47:23 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const router = require("./router");
+const PORT = process.env.PORT || 5000;
+
+io.on("connection", (socket) => {
+  console.log("New Connection");
+});
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
